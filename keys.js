24de32b8982639372c8c892861a3a555a389e3cd
marie <marie.delborrello11@gmail.com>
2019-11-26 14:19:28 +0800

    first root files

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.SPOTIFY_ID="55dfa89ad3d247f3a5cd15ce1614b056",
+  secret: process.env.SPOTIFY_SECRET="dced4b2b6c744dbf83b7b3e38c364e1e"
 };
